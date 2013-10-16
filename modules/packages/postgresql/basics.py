96e037f7e88ed4a8312e0f6323ac7e0a5d0fd968
ccat <deadlack@gmail.com>
2013-10-16 22:08:37 +0900

    modified

--- modules/packages/postgresql/basics.py
+++ modules/packages/postgresql/basics.py
@@ -7,27 +7,27 @@ class Postgresql(interfaces.Package):
         """ Setting default parameters based on ubuntu.
         """
         interfaces.Package.__init__(self, packageName,server)
-        
+
         self.username="postgres"
         self.bin_path=None#"/usr/lib/postgresql/9.1/bin"
         self.dbClusters={}#{"default":{"maindir":"","configdir":"","encoding":"UTF8","locale":None,"superuser":"postgres"}}
         self.version=None
         self.installed=None
-        
+
         self.lowlevel=Lowlevel(self)
 
     def addDBcluster(self,clusterName,dir,encoding="UTF8",locale=None,username="postgres"):
         """ Creating new DB cluster configure.  This function is not create actual DB cluster.
             When initdb will be run and actual DB cluster will be created.
-            
+
         """
         temp={"dir":dir,"encoding":encoding,"locale":locale,"superuser":username}
         self.dbClusters[clusterName]=temp
-    
+
     def configure(self,version=None):
         if(version!=None):
             self.version=version
-            
+
     def diagnosis(self):
         result={}
         level="NORMAL"
@@ -48,7 +48,7 @@ class Postgresql(interfaces.Package):
             insLevel="ERROR"
             level="ERROR"
         result["installed"]={"level":insLevel,"config":self.installed,"status":installed}
-        
+
         result["level"]=level
         return result
 
@@ -76,7 +76,7 @@ class Postgresql(interfaces.Package):
             self.dbClusters["default"]={"maindir":None,"configdir":None,"encoding":None,"locale":None,"superuser":"postgres"}
 
         return level,result
-        
+
     def version_check(self):
         result=self.os().lowlevel.exec_command(["psql","--version"],raiseOnError=True)
         versionLine=result["stdout"].split("\n")[0]
@@ -84,7 +84,7 @@ class Postgresql(interfaces.Package):
         return version
         #psql (PostgreSQL) 9.1.9
 
-        
+
     def install(self):
         if(self.os().distribution_name=="CentOS"):
             self.__install_centos__()
@@ -96,7 +96,7 @@ class Postgresql(interfaces.Package):
         #    return self.__install_debian__()
         #if(self.os()=="Fedora"):
         #    return self.__install_fedora__()
-        
+
     def __install_ubuntu__(self):
         if(self.version==None):
             result=self.os().lowlevel.exec_command(["apt-get", "-y", "install", "postgresql"])
@@ -119,7 +119,7 @@ class Postgresql(interfaces.Package):
         result=self.os().lowlevel.exec_command("echo \"deb http://apt.postgresql.org/pub/repos/apt/ "+varName+"-pgdg main\" > /etc/apt/sources.list.d/pgdg.list",shell=True)
         if(result["returncode"]!=0):
             raise Exception(result)
-    
+
         result=self.os().lowlevel.exec_command("wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -",shell=True)
         if(result["returncode"]!=0):
             raise Exception(result)
@@ -127,13 +127,13 @@ class Postgresql(interfaces.Package):
         result=self.os().lowlevel.exec_command(["apt-get", "update"])
         if(result["returncode"]!=0):
             raise Exception(result)
-    
+
         result=self.os().lowlevel.exec_command(["apt-get","-y", "install", "postgresql-"+self.version])
         if(result["returncode"]!=0):
             raise Exception(result)
 
 
-    
+
     def __install_centos__(self):
         if(self.version==None):
             result=self.os().yum_install("postgresql-server")
@@ -145,7 +145,7 @@ class Postgresql(interfaces.Package):
         elif(self.version=="9.1"):
             self.__install_centos_91__()
         self.__install_centos_post_installation__()
-        
+
     def __install_centos_93__(self):
         tempVersion=self.os().distribution_version.split(".")[0]
         self.os().rpm_i("http://yum.postgresql.org/9.3/redhat/rhel-"+tempVersion+"-"+self.os().hardware_type+"/pgdg-centos93-9.3-1.noarch.rpm")
@@ -160,14 +160,14 @@ class Postgresql(interfaces.Package):
         tempVersion=self.os().distribution_version.split(".")[0]
         self.os().rpm_i("http://yum.postgresql.org/9.1/redhat/rhel-"+tempVersion+"-"+self.os().hardware_type+"/pgdg-centos91-9.1-4.noarch.rpm")
         result=self.os().yum_install("postgresql93-server")
-        
+
     def __install_centos_post_installation__(self):
         command=None
         if(self.version==None):
             command="postgresql"
         else:
             command="postgresql-"+self.version
-            
+
         result=self.os().lowlevel.exec_command(["service",command,"initdb"])
         if(result["returncode"]!=0):
             raise Exception(result)
@@ -200,7 +200,7 @@ class Lowlevel(object):
                 It is better to disable locale.
             -U / --username Specify superuser name of the DB cluster.
         """
-        #sudo -u postgres 
+        #sudo -u postgres
         dbCluster=self.postgresql.dbClusters[dbClusterName]
         command=["sudo","-u",self.postgresql.username]
         command.append(self.postgresql.bin_path+"/initdb")
@@ -209,7 +209,7 @@ class Lowlevel(object):
             command.append(dbCluster["dir"])
         if(dbCluster["encoding"]!=""):
             command.append("-E")
-            command.append(dbCluster["encoding"])            
+            command.append(dbCluster["encoding"])
         if(dbCluster["locale"]==None):
             command.append("--no-locale")
         else:
@@ -217,7 +217,7 @@ class Lowlevel(object):
         if(dbCluster["superuser"]!=""):
             command.append("-U")
             command.append(dbCluster["superuser"])
-       
+
         return self.postgresql.os().lowlevel.exec_command(command)
 
     def pg_dumpall(self,filename,cluster="default",withClean=False,withColumnInsert=False,withCompress=False):
@@ -225,13 +225,13 @@ class Lowlevel(object):
         withClean : Output SQL includes drop commands for existing databases.
         withColumnInsert : Output SQL uses insert instead of copy.
         withCompress : Outpu SQL is compressed by gzip
-        
+
         if filename inclueds "%num%", it will be changed as YYYYMMDD_number
         """
         if(cluster!="default"):
             raise Exception("None default cluster backup by pg_dumpall is not implemented now.")
         #dbCluster=self.postgresql.dbClusters[cluster]
-        
+
         if(filename.find("%num%")!=-1):
             import datetime
             ymd=datetime.date.today().strftime("%Y%m%d")
@@ -242,28 +242,28 @@ class Lowlevel(object):
                 num=num+1
                 tempfilename=filename.replace("%num%",ymd+"_"+str(num))
             filename=tempfilename
-        
+
         #command=["sudo","-u",self.postgresql.username,"pg_dumpall","--file="+filename,"-w"]
         commandTemp="pg_dumpall -w"
         if(withClean):
-            commandTemp=commandTemp+" -c" 
+            commandTemp=commandTemp+" -c"
         if(withColumnInsert):
             commandTemp=commandTemp+" --column-inserts"
-        
+
         commandTemp=commandTemp+" > /tmp/backup.sql"
-        
+
         command=[commandTemp]
-        
+
         if(withCompress):
-            command.append("gzip -c > /tmp/backup.sql.gz")
+            command.append("gzip -c /tmp/backup.sql > /tmp/backup.sql.gz")
+            command.append("rm -f /tmp/backup.sql")
             command.append("mv /tmp/backup.sql.gz "+filename)
         else:
             command.append("mv /tmp/backup.sql "+filename)
-        command.append("rm -f /tmp/backup.sql")
         #result=self.postgresql.os().lowlevel.exec_command(command,shell=True,raiseOnError=True)
         result=self.postgresql.os().lowlevel.exec_script(command,runuser=self.postgresql.username,raiseOnError=True)
         return result
-    
+
     def psql_restore(self,filename,cluster="default",withCompress=False):
         if(cluster!="default"):
             raise Exception("None default cluster backup by pg_dumpall is not implemented now.")
@@ -275,7 +275,7 @@ class Lowlevel(object):
 
         result=self.postgresql.os().lowlevel.exec_command(command,shell=True,raiseOnError=True)
         return result
-    
+
     #(cluster="default",filename="/tmp/backup.sql")
 
 class Pg_ctl(object):
@@ -289,7 +289,7 @@ class Pg_ctl(object):
                 When -D is not specified and $PGDATA will be used.
             -o Options for initdb
         """
-        #sudo -u postgres 
+        #sudo -u postgres
         dbCluster=self.postgresql.dbClusters[dbClusterName]
         command=["sudo","-u",self.postgresql.username]
         command.append(self.postgresql.bin_path+"/pg_ctl")
@@ -297,10 +297,10 @@ class Pg_ctl(object):
         if(dbCluster["dir"]!=""):
             command.append("-D")
             command.append(dbCluster["dir"])
-        
+
         options=""
         if(dbCluster["encoding"]!=""):
-            options=" -E "+dbCluster["encoding"]            
+            options=" -E "+dbCluster["encoding"]
         if(dbCluster["locale"]==None):
             options=options+" --no-locale"
         else:
@@ -309,7 +309,7 @@ class Pg_ctl(object):
             options=options+" -U "+dbCluster["superuser"]
         command.append("-o")
         command.append(options)
-       
+
         return self.postgresql.os().lowlevel.exec_command(command)
 
 
